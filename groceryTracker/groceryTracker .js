e5d5281d75b1f981498a2efbe0d75eed3e158769
julienmandereau <julien.mandereau@orange.com>
2025-11-10 07:12:54 -0500

    sauv

---
+++ groceryTracker/groceryTracker .js
@@ -0,0 +1,12 @@
+let art1;
+let art2;
+let art3;
+function total() {
+ art1 = parseFloat(document.getElementById('art1').value);
+ art2 = parseFloat(document.getElementById('art2').value);
+ art3 = parseFloat(document.getElementById('art3').value);
+
+ let total = lenart1 + art2 +art3 ;
+  document.getElementById('result').innerText = `Merci de me payer: ${total}`;
+}
+
