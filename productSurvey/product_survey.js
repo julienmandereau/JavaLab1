a761c12bbdedd015f3559c40d50593ace3beff0d
julienmandereau <julien.mandereau@orange.com>
2025-11-11 06:38:17 -0500

    sauv

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperince = document.getElementById('userExperince').value;
 
 
     document.getElementById('userName').innerHTML = username;
@@ -15,6 +16,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = userExperince;
 
     document.getElementById('userInfo').style.display = 'block';
 }
