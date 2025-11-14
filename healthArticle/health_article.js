8ff64b7158a28ceb6ad8ab06d10e4c1b6e2eeaba
julienmandereau <julien.mandereau@orange.com>
2025-11-14 06:10:48 -0500

    sauv

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -6,8 +6,10 @@ xhr.open('GET', url, true);
 
 xhr.responseType = 'json';
 
+xhr.onload = function() {
 var articles = xhr.response.articles;
 var articlesDiv = document.getElementById('articles');
+
 
   articles.forEach(function(article) {
       var articleDiv = document.createElement('div');
@@ -48,6 +50,7 @@ var articlesDiv = document.getElementById('articles');
 
       articlesDiv.appendChild(articleDiv);
     });
+    }
 xhr.send();
 
     
